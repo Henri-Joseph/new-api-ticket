81df5157bc377c7760222911dd325054d6df1366
Henri-Joseph <122472490+Henri-Joseph@users.noreply.github.com>
2024-06-17 13:52:47 +0000

    Base de données
    Configuration de la base de données sous postgres

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Fonction calculatrice
+"""
+
+def add(x, y):
+    """Ajoute X et Y pour obtenir le resultat."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
