fef95d00c2faaae7f2edc07d0727d1769d47063a
Henri-Joseph <122472490+Henri-Joseph@users.noreply.github.com>
2024-06-17 14:29:56 +0000

    Fixed test.py

--- app/app/test.py
+++ app/app/test.py
@@ -5,7 +5,6 @@ from django.test import SimpleTestCase
 
 from app import calc
 
-
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
@@ -21,4 +20,3 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
-
