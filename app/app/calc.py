8f23fcffdde4804f0bee4882f6a678808a3ec592
Henri-Joseph <122472490+Henri-Joseph@users.noreply.github.com>
2024-06-17 14:14:20 +0000

    Fixed

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,5 +1,4 @@
-"""Fonction calculatrice
-"""
+"""Fonction calculatrice"""
 
 def add(x, y):
     """Ajoute X et Y pour obtenir le resultat."""
@@ -9,3 +8,4 @@ def add(x, y):
 def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
+
